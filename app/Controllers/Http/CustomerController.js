8f51f6e66d1f0e1efb654068bce2056358d36ee5
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-08-05 09:47:58 -0300

    Cria store de clientes

--- app/Controllers/Http/CustomerController.js
+++ app/Controllers/Http/CustomerController.js
@@ -1,12 +1,41 @@
 'use strict'
 
 const Customer = use('App/Models/Customer');
-
+const { validate } = use('Validator')
 class CustomerController {
 
     async index ({ auth }) {
         const user = await auth.getUser()
         return await user.customers().fetch()
+    }
+
+    async store ({ request, response, auth }) {
+        const user = await auth.getUser()
+        const data = request.only(['name'])
+
+        const rules = {
+            name: 'string|required|max:80',
+        }
+
+        const validation = await validate(data, rules)
+
+        if (validation.fails()) {
+            return response.status(400)
+              .json(validation.messages());
+        }
+
+        let customer = new Customer()
+
+        customer.fill(data);
+
+        try {
+          await user.customers().save(customer)
+          return customer
+        } catch(err) {
+          return response.status(500).json({
+            message: err.message,
+          })
+        }
     }
 }
 
