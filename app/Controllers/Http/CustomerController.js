8084384ecdf80b1bbfc23200f1250e5f446a6236
Raphael Batagini <raphael.batagini@i9xp.com.br>
2020-08-11 09:24:33 -0300

    Ajusta indentacao

--- app/Controllers/Http/CustomerController.js
+++ app/Controllers/Http/CustomerController.js
@@ -1,42 +1,42 @@
 'use strict'
 
-const Customer = use('App/Models/Customer');
+const Customer = use('App/Models/Customer')
 const { validate } = use('Validator')
+
 class CustomerController {
+  async index ({ auth }) {
+    const user = await auth.getUser()
+    return await user.customers().fetch()
+  }
 
-    async index ({ auth }) {
-        const user = await auth.getUser()
-        return await user.customers().fetch()
+  async store ({ request, response, auth }) {
+    const user = await auth.getUser()
+    const data = request.only(['name'])
+
+    const rules = {
+      name: 'string|required|max:80',
     }
 
-    async store ({ request, response, auth }) {
-        const user = await auth.getUser()
-        const data = request.only(['name'])
+    const validation = await validate(data, rules)
 
-        const rules = {
-            name: 'string|required|max:80',
-        }
-
-        const validation = await validate(data, rules)
-
-        if (validation.fails()) {
-            return response.status(400)
-              .json(validation.messages());
-        }
-
-        let customer = new Customer()
-
-        customer.fill(data);
-
-        try {
-          await user.customers().save(customer)
-          return customer
-        } catch(err) {
-          return response.status(500).json({
-            message: err.message,
-          })
-        }
+    if (validation.fails()) {
+      return response.status(400)
+        .json(validation.messages())
     }
+
+    let customer = new Customer()
+
+    customer.fill(data)
+
+    try {
+      await user.customers().save(customer)
+      return customer
+    } catch(err) {
+      return response.status(500).json({
+        message: err.message,
+      })
+    }
+  }
 }
 
 module.exports = CustomerController
